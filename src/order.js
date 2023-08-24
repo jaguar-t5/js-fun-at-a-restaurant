962f7e250801de6dad704d0328e0f8926224d566
Brendan Turner <kisbyturner@gmail.com>
2023-08-24 15:23:27 -0600

    complete searchOrder function in order.js

--- src/order.js
+++ src/order.js
@@ -16,9 +16,31 @@ function refundOrder(orderNum, deliveryOrders){
     }
   }
 }
+
+function listItems(deliveryOrders) {
+  var items = "";
+  for(i = 0; i < deliveryOrders.length; i++) {
+    if(i < deliveryOrders.length-1) {
+      items += `${deliveryOrders[i].item}, `;
+    }
+    else {
+      items += `${deliveryOrders[i].item}`;
+    }
+  }
+  return items
+}
+
+function searchOrder(deliveryOrders, item) {
+  for(i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].item === item) {
+      return true
+    }
+  }
+  return false
+}
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }
